8a7c9a6f9e54321c27eb7966a302d7115dc02052
harshalc108 <95841542+harshalc108@users.noreply.github.com>
2025-06-10 02:18:40 +0530

    DT-7: Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "google" {
+  project = "skilful-asset-462308-a0"
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "terraform_state" {
+  name     = "terraform-state-dev"
+  location = "US"
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  uniform_bucket_level_access = true
+}
