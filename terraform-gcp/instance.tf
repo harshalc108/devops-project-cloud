5b7a126bd1627a0c683b39136efa4b4af1ab2068
harshalc108 <harshalchavhan108@gmail.com>
2025-06-16 01:21:56 +0530

    dt-25:removed worksoft servers

--- terraform-gcp/instance.tf
+++ terraform-gcp/instance.tf
@@ -6,12 +6,3 @@ module "sonarqube" {
   tags           = ["terraform", "dev"]
 
 }
-
-module "worksoft" {
-  source         = "./modules/compute_instance"
-  instance_name  = "wpec5009wksft"
-  replica_count  = 2
-  zone           = var.zone
-  tags           = ["terraform", "dev","worksoft"]
-
-}
